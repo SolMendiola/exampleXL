0badfd54d422a4d5ab2e1c38e7f1bf55376aa919
Sol Mendiola <solmendiola73@gmail.com>
2022-09-20 14:30:07 -0300

    srtated TODO app

---
+++ to_do_app/lib/cardToDo.dart
@@ -0,0 +1,9 @@
+import 'dart:core';
+
+class CardToDo {
+    String title;
+    String description;
+    bool done;
+
+    CardToDo({required this.title, required this.description, required this.done});
+}
